bea1ffb46d5fc608fd3d58c108c757506bf35ae7
Devang kushwah <deekayedm@gmail.com>
2024-02-13 21:52:40 +0530

    writing backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express();
+
+app.use(express.json())
+
+
+
+app.post("/todos",(req,res)=>{
+
+})
+
+app.get("/todos",(req,res)=>{
+
+})
+
+app.put("/complete",(req,res)=>{
+
+})
+
+app.listen(3000,()=>{
+    console.log("server is running")
+})
