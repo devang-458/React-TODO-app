c94d7cc6f58684beea70ef269285a2c794d3716d
Devang kushwah <deekayedm@gmail.com>
2024-02-13 22:18:04 +0530

    done type.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
 
+
 app.use(express.json())
 
 
@@ -10,7 +11,7 @@ app.post("/todos",(req,res)=>{
 })
 
 app.get("/todos",(req,res)=>{
-
+ 
 })
 
 app.put("/complete",(req,res)=>{
